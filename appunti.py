4cf4f11a302e594f0a90ff2f2e55b730b34e915c
Dobinda-Alessandro <dobinda.alessandro@iisgalvanimi.edu.it>
2023-01-12 12:02:06 +0000

    lezione

---
+++ appunti.py
@@ -0,0 +1,29 @@
+# doc type è importante perchè definisce il tipo di file 
+#Importante bisogna sempre chiudere i Tags 
+#html tags 
+#<html></html>#
+#<head>
+#<body>
+#<h1> le scritte più grandi che puoi fare 
+#<p> per i parafragi 
+#<div> posso mettere elementi in un posto solo le mette nella stessa riga 
+#<spain> stessa cosa ma non le mette in linea 
+#ATTRIBUTI 
+#<tagname abc=""> contenuto all'interno di tag dove possiamo inserire valore attributi
+#Links 
+#<a> è un  link 
+#per collegare a qualcosa devi usare l'attributo 
+#<a href="qui va il link"> href sta per che si riferisce ad un altro sito
+#<a href="link di tik tok ">link to tiktok> la seconda tag fa vedere la frase che voglio mettere , si usa per tutti i link 
+# immagini , imaggine è una eccezione non va chiuso con un tag a parte ma va chiuyso subito perchè non ci possono essere attributi dentro <img src="venecia.jpg"/> non me la data perche non ho la fonte devo salvare l'immagine e mettere dentro comne ho salvato il file e la fonte es .jpg
+# widht="50"
+# height
+# app/
+#   hello.html
+#       images/
+# devo mettere  <img src="immagini/no.jpg" il nome del folder 
+#           venecia.jpg        se metto immagine in un folder cosi posso mettere più immagini 
+# lista 
+# <ol> lista ordinata 
+#<ul> lista non ordinata 
+#<il> lista item elementi 
